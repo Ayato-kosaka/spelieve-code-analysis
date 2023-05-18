15a55f7a2e7dfe08f224a2966412520c98645093
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-05-18 21:32:27 +0000

    first commit

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const rootDir = "../spelieve/";
+export const repoBaseUrl =
+  "https://github.com/Ayato-kosaka/spelieve/blob/develop";
---
+++ src/utils.ts
@@ -0,0 +1,43 @@
+import { Project, Node } from "ts-morph";
+import * as fs from "fs";
+import * as path from "path";
+import { repoBaseUrl, rootDir } from "./config";
+
+// ts-morphのProjectインスタンスを作成
+export const project = new Project({
+  tsConfigFilePath: rootDir + "tsconfig.json",
+});
+
+function safeStringify(obj: any): string {
+  const cache = new Set();
+
+  return JSON.stringify(obj, (_, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (cache.has(value)) {
+        // Circular reference found, discard key
+        return;
+      }
+      cache.add(value);
+    }
+    return value;
+  });
+}
+
+// Nodeをファイルに書き出す
+export function writeNode(node: Node, file_suffix_variable?: string) {
+  const filename =
+    "logs/extracted_node" +
+    (file_suffix_variable ? "_" + file_suffix_variable : "") +
+    ".json";
+  const safeJson = safeStringify(node.compilerNode);
+  fs.writeFileSync(filename, safeJson);
+  console.log(`Extracted Node saved to: ${filename}`);
+}
+
+export function getRelativePathFromAbsolutePath(absolutePath: string) {
+  return path.relative(rootDir, absolutePath);
+}
+
+export function getRepoFilePathFromAbsolutePath(absolutePath: string) {
+  return path.join(repoBaseUrl, getRelativePathFromAbsolutePath(absolutePath));
+}
